6a410a890afc25d3cfa13b0d9824a0bc79c4d50b
jvc perseu <victorjvc2020@gmail.com>
2025-07-24 11:08:49 -0300

    refatorei a função geradora das supercelulas

--- scripts/get_supercell_dir.py
+++ scripts/get_supercell_dir.py
@@ -10,36 +10,32 @@ from ase.io.espresso import write_espresso_in
 
 
 
-def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float, cellsize: Tuple[int, int, int]):
-    """
-   Create a supercell input file for Quantum ESPRESSO based on an existing input file.
-   Args:
-        input_file_path (str): Path to the existing input file (e.g., ZnO_template.in).
-        a (float): Lattice parameter 'a' in Angstroms.
-        covera (float): c/a ratio for the wurtzite structure.
-        cellsize (Tuple[int, int, int]): Tuple representing the supercell size in terms of unit cells (nx, ny, nz).
-   Returns:
-        None: The function creates a directory and writes the input file for the supercell. 
-    """
+def make_pw_from_template(
+    template_path: str,
+    a: float,
+    covera: float,
+    cellsize: Tuple[int, int, int]
+    ) -> None:
+    """Create a supercell input file for Quantum ESPRESSO based on an existing input file."""
     
-    # Primitive wurtzite cell for ZnO from scripts/ZnO_template.in
-    primitive_cell = read(input_file_path, format='espresso-in') 
+    
+    primitive_cell = read(template_path, format='espresso-in') 
     nx, ny, nz = cellsize
     supercell_matrix = np.array([
         [nx, 0, 0],
         [0, ny, 0],
-        [0, 0, nz] ]
-    )
+        [0, 0, nz] 
+    ])
     supercell = make_supercell(prim=primitive_cell, P=supercell_matrix)
     
-    # Create the directory for the supercell type
+    
     cell_dirname = f'cell{nx}{ny}{nz}.in'
     os.makedirs(cell_dirname, exist_ok=True)
 
     # Input file name based on the parameters
     input_name: str = f'ZnO-{a:.2f}-{covera:.2f}-{nx}{ny}{nz}.in'
-    print(f'Produzindo : {input_name}')
     
+    print(f'Produzindo : {input_name}')
     
     HEADER_INPUT = {
     'control': {
@@ -65,9 +61,9 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
     PSEUDOS = {'Zn': 'Zn.upf','O': 'O.upf'}
     
     # Adapt K-grid to larger supercells, mantaining grid density
-    kpoints_original = np.array([6, 6, 6])  
-    kpoints_supercell = kpoints_original // np.array(cellsize)  
-    kpoints_supercell[kpoints_supercell == 0] = 1 
+    KPOITNS_ORIGINAL = np.array([6, 6, 6])  
+    KPOINT_SUPERCELL = KPOITNS_ORIGINAL // np.array(cellsize)  
+    KPOINT_SUPERCELL[KPOINT_SUPERCELL == 0] = 1 
 
     
     write_espresso_in(
@@ -75,7 +71,7 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
         atoms = supercell,
         input_data = HEADER_INPUT,
         pseudopotentials = PSEUDOS,
-        kpts = tuple(kpoints_supercell),
+        kpts = tuple(KPOINT_SUPERCELL),
         koffset = (0,0,0),
         crystal_coordinates = True # TODO: check if this is needed
     )
@@ -84,7 +80,7 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
 if  __name__ == "__main__":
     percent_range = np.arange(start= -0.10, stop=0.12, step=0.02)
 
-    # vc-relax data from ZnO_system_preparation/data/vc-relax2
+    # Relaxed structure parameters for ZnO
     CELLDM1_Bohr_units = 6.178821408099141            
     CELLDM1_angstroms = CELLDM1 * 0.52918  
     CELLDM3 = 1.614358356153010 
@@ -100,8 +96,10 @@ if  __name__ == "__main__":
     # ================ STRAIN =================== #
     for a in a_values:
         for covera in c_values:
-            make_pw_input_from_existing_in(input_file_path=template_path,
+            make_pw_from_template(
+                template_path=template_path,
                 a=a,
                 covera=covera,
-                cellsize=(2, 2, 2))
+                cellsize=(2, 2, 2)
+                )
 
