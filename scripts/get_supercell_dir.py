b59cdc67bcded5625c43794ed6194565b5a64842
jvc perseu <victorjvc2020@gmail.com>
2025-07-24 10:21:31 -0300

    Remoção comentários desnecessários

--- scripts/get_supercell_dir.py
+++ scripts/get_supercell_dir.py
@@ -21,10 +21,9 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
    Returns:
         None: The function creates a directory and writes the input file for the supercell. 
     """
-    # OLD (wrong): bulk(name='ZnO', crystalstructure='wurtzite', a=a, covera=covera) 
-    # --> creates convencional cell (hexagonal with 8 atoms)#
-
-    primitive_cell = read(input_file_path, format='espresso-in') # Primitive wurtzite cell for ZnO from scripts/ZnO_template.in
+    
+    # Primitive wurtzite cell for ZnO from scripts/ZnO_template.in
+    primitive_cell = read(input_file_path, format='espresso-in') 
     nx, ny, nz = cellsize
     supercell_matrix = np.array([
         [nx, 0, 0],
@@ -32,6 +31,7 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
         [0, 0, nz] ]
     )
     supercell = make_supercell(prim=primitive_cell, P=supercell_matrix)
+    
     # Create the directory for the supercell type
     cell_dirname = f'cell{nx}{ny}{nz}.in'
     os.makedirs(cell_dirname, exist_ok=True)
@@ -40,7 +40,7 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
     input_name: str = f'ZnO-{a:.2f}-{covera:.2f}-{nx}{ny}{nz}.in'
     print(f'Produzindo : {input_name}')
     
-    # Input comum para todas as simulações
+    
     HEADER_INPUT = {
     'control': {
         'calculation': 'scf',
@@ -67,11 +67,11 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
     # Adapt K-grid to larger supercells, mantaining grid density
     kpoints_original = np.array([6, 6, 6])  
     kpoints_supercell = kpoints_original // np.array(cellsize)  
-    kpoints_supercell[kpoints_supercell == 0] = 1 # Ensure no zero k-points;
+    kpoints_supercell[kpoints_supercell == 0] = 1 
 
-    # Write the input file using ASE's espresso interface
+    
     write_espresso_in(
-        file = os.path.join(cell_dirname, input_name), # One folder per supercell
+        file = os.path.join(cell_dirname, input_name), 
         atoms = supercell,
         input_data = HEADER_INPUT,
         pseudopotentials = PSEUDOS,
@@ -80,32 +80,22 @@ def make_pw_input_from_existing_in(input_file_path: str, a: float, covera: float
         crystal_coordinates = True # TODO: check if this is needed
     )
 
-# ============= Structural Parameters ============== #
-percent_range = np.arange(start= -0.10, stop=0.12, step=0.02)
-
-# vc-relax data from ZnO_system_preparation/data/vc-relax2
-CELLDM1 = 6.178821408099141            # in Bohr units
-CELLDM1_Angstroms = CELLDM1 * 0.52918  # in Angstroms
-CELLDM3 = 1.614358356153010 # Adimensional c/a ratio
-
-# Strained parameters - FUTURE
-a_values = CELLDM1_Angstroms * (1 + percent_range)
-c_values = CELLDM3 * (1 + percent_range)
-# ================================================ #
-
 
 if  __name__ == "__main__":
-    # Path to the template input file - get 1x1x1 atoms structure for ASE.
+    percent_range = np.arange(start= -0.10, stop=0.12, step=0.02)
+
+    # vc-relax data from ZnO_system_preparation/data/vc-relax2
+    CELLDM1_Bohr_units = 6.178821408099141            
+    CELLDM1_angstroms = CELLDM1 * 0.52918  
+    CELLDM3 = 1.614358356153010 
+
+    # Strained parameters - FUTURE
+    a_values = CELLDM1_Angstroms * (1 + percent_range)
+    c_values = CELLDM3 * (1 + percent_range)
+    
+    # Path to the template input file - 1x1x1 atoms structure for ASE.
     template_path = 'ZnO_template.in'  
    
-   # (1,1,1) to (3,3,3) supercells
-    #for nx in range(1, 4):  
-    #    for ny in range(1, 4):  
-    #        for nz in range(1, 4):
-    #            make_pw_input_from_existing_in(input_file_path=template_path,
-    #                a=CELLDM1_Angstroms,
-    #                covera=CELLDM3,
-    #                cellsize=(nx, ny, nz) )
     
     # ================ STRAIN =================== #
     for a in a_values:
@@ -113,5 +103,5 @@ if  __name__ == "__main__":
             make_pw_input_from_existing_in(input_file_path=template_path,
                 a=a,
                 covera=covera,
-                cellsize=(3, 1, 1))
+                cellsize=(2, 2, 2))
 
